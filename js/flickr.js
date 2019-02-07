a6c6039abc8507a0b0986603833c1032b00624b1
Chris Brinker <cmbrinker@gmail.com>
2019-02-07 17:33:13 -0500

    getJSON photo tags

--- js/flickr.js
+++ js/flickr.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
   $('button').click(function() {
     $('button').removeClass('selected');
     $(this).addClass('selected');
+    var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+    var filter = $(this).text();
+    var flickrData = {
+      tags: filter
+    };
+    function displayPhotos (data) {
+      
+    };
+    $.getJSON(flickrAPI, flickrData, displayPhotos);
   });
 });
