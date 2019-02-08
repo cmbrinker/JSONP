88acd29e97bd86cd893980d073fd5436ccd9fabd
Chris Brinker <cmbrinker@gmail.com>
2019-02-08 11:49:08 -0500

    JSON flickr success

--- js/flickr.js
+++ js/flickr.js
@@ -5,10 +5,18 @@ $(document).ready(function() {
     var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
     var filter = $(this).text();
     var flickrData = {
-      tags: filter
+      tags: filter,
+      format: 'json'
     };
     function displayPhotos (data) {
-      
+      var photoHTML = '<ul>';
+      $.each(data.items, function(i, photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
     };
     $.getJSON(flickrAPI, flickrData, displayPhotos);
   });
