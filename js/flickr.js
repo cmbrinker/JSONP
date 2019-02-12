08c75ffa72393aaec031be11ca4110f39aa6d983
Chris Brinker <cmbrinker@gmail.com>
2019-02-11 22:16:43 -0500

    search field addded

--- js/flickr.js
+++ js/flickr.js
@@ -1,23 +1,45 @@
+// $(document).ready(function() {
+//   $('button').click(function() {
+//     $('button').removeClass('selected');
+//     $(this).addClass('selected');
+//     var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+//     var filter = $(this).text();
+//     var flickrData = {
+//       tags: filter,
+//       format: 'json'
+//     };
+//     function displayPhotos (data) {
+//       var photoHTML = '<ul>';
+//       $.each(data.items, function(i, photo) {
+//         photoHTML += '<li class="grid-25 tablet-grid-50">';
+//         photoHTML += '<a href="' + photo.link + '" class="image">';
+//         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+//       });
+//       photoHTML += '</ul>';
+//       $('#photos').html(photoHTML);
+//     };
+//     $.getJSON(flickrAPI, flickrData, displayPhotos);
+//   });
+// });
+
 $(document).ready(function() {
   $('button').click(function() {
-    $('button').removeClass('selected');
-    $(this).addClass('selected');
-    var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
-    var filter = $(this).text();
-    var flickrData = {
-      tags: filter,
+    var url = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+    var searchData = $('#search').text();
+    var sentData = {
+      tags: searchData,
       format: 'json'
     };
-    function displayPhotos (data) {
+    function callback (flickrData) {
       var photoHTML = '<ul>';
-      $.each(data.items, function(i, photo) {
+      $.each(flickrData.items, function(index, photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        photoHTML += '<img src="' + photo.media.m + '" class=image>';
+        photoHTML += '<a href="' + photo.link + '"></a></li>';
       });
       photoHTML += '</ul>';
       $('#photos').html(photoHTML);
     };
-    $.getJSON(flickrAPI, flickrData, displayPhotos);
-  });
+    $.getJSON(url, sentData, callback);
+});
 });
